0cf672452268a80dba55ef31a047cfd966fdc69e
slay3434 <slay3434@o2.pl>
2018-10-18 21:43:50 +0200

    php artisan make:migration create_links_table --create=links

---
+++ database/migrations/2018_10_18_190947_create_links_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('title');
+            $table->string('url')->unique();
+            $table->text('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+    
+    
+}
